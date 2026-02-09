9533f1d33e32b66731717c4485e6f585ed4c270f
wende12github <wendmagegn9412@gmail.com>
2026-02-09 16:47:32 +0300

    Screen Page export Implemented to access the code and reuse it

--- portfolio-site/src/components/screens/index.ts
+++ portfolio-site/src/components/screens/index.ts
@@ -1,1 +1,8 @@
 export { HeroSection } from "./HeroSection";
+export { AboutSection } from "./AboutSection";
+export { SkillsSection } from "./SkillsSection";
+export { ProjectsSection } from "./ProjectsSection";
+export { CertificatesSection } from "./CertificatesSection";
+export { ContactSection } from "./ContactSection";
+export { BlogSection } from "./BlogSection";
+export { ExperienceSection } from "./ExperienceSection";
