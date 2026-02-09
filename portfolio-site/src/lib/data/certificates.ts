c369ab32f1aeddba5eec7418685710b1c08699a1
wende12github <wendmagegn9412@gmail.com>
2026-02-09 22:40:52 +0300

    Implemented The Certificate common data to make the code consistent and clear

---
+++ portfolio-site/src/lib/data/certificates.ts
@@ -0,0 +1,66 @@
+import type { Certificate } from "@/types"
+
+
+
+export const CERTIFICATES: Certificate [] = [
+    {
+        id: '1',
+        title: 'AASTU Tech Fest 2nd place Hackathon Winner',
+        issuer: 'Addis Ababa Science and Technology University',
+        date: '2025',
+        description: 'Second place winner in the 2025 AASTU Tech Fest Hackathon for developing an innovative Digital Skill Sharing and AI Integrated solution.',
+        image: 'images/certificates/2025-hackathon.jpg',
+        credentialUrl: 'https://drive.google.com/file/d/1ZgUyw95TUyEeYbcypafYBVni244zvpnO/view?usp=sharing',
+        featured: true,
+    },
+    {
+        id: '2',
+        title: 'Django Backend Development',
+        issuer: 'GDG Addis Ababa Science and Technology University',
+        date: '2025',
+        description: 'Professional certification in modern Django Backend web development covering Django, DRF, and databases PostgreSql.',
+        image: 'images/certificates/Django-GDG-certificate.jpg',
+        credentialUrl: 'https://drive.google.com/file/d/1SJI12cwzAnMtaf9OQincC3cvwBwlTN-y/view?usp=sharing',
+        featured: false
+    },
+    {
+        id: '3',
+        title: 'A2SV 2024 AI for Impact Hackathon',
+        issuer: 'Africa to Silicon Valley (A2SV)',
+        date: '2024',
+        description: 'Certification demonstrating proficiency in building and training Sign Language Translator using TensorFlow and React Native with Flask Backend.',
+        image: 'images/certificates/a2sv-hackathon.png',
+        credentialUrl: 'https://drive.google.com/file/d/12Acxzd7dyRIePPafugP5jUuIEQ_b4Far/view?usp=sharing',
+        featured: false
+    },
+    {
+        id: '4',
+        title: '3rd Cyber Talent Summer Camp',
+        issuer: 'Information Network Security Agency (INSA)',
+        date: '2024 GC / 2016 EC',
+        description: 'Certification for successful completion of the 3rd Cyber Talent Summer Camp organized by INSA, covering cybersecurity fundamentals, ethical hacking, and network defense.',
+        image: 'images/certificates/INSA-cybertalent-3rd.jpg',
+        credentialUrl: 'https://drive.google.com/file/d/1ES4fjCEKdSK-8t_YkT4oKvhzg8gG23y5/view?usp=sharing',
+        featured: false
+    },
+    {
+        id: '5',
+        title: 'Android Developer Nanodegree',
+        issuer: 'Udacity',
+        date: 'August 29 2024',
+        description: 'Certification for completing the Android Developer Nanodegree program, demonstrating proficiency in Android app development using Java and Kotlin.',
+        image: 'images/certificates/Android-dev-udacity.png',
+        credentialUrl: 'https://drive.google.com/file/d/1uQk6XLibORGQ3jiKOdGOY4BJeSFN416B/view?usp=sharing',
+        featured: false
+    },
+    {
+        id: '6',
+        title: 'Python for Data Science',
+        issuer: 'Addis Ababa Science and Technology University with Skill Spectrum',
+        date: '2023-2024',
+        description: 'Certification for completing the Python for Data Science course, covering Python programming, data analysis, and visualization techniques.',
+        image: 'images/certificates/pythonCerti.jpg',
+        credentialUrl: 'https://drive.google.com/file/d/12Pbw0pFiElUpuCSPAucDu4TiVvtxn2JW/view?usp=sharing',
+        featured: false
+    }
+]
